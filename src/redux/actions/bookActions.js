97b4135a27f140c15f49018a5fea057a9be2fccc
MuhammadHafijur <hafijur.salah@gmail.com>
2022-08-26 11:15:26 +0600

    useSelector hook

---
+++ src/redux/actions/bookActions.js
@@ -0,0 +1,16 @@
+
+
+export function addToReadingList(payload){
+    return {
+        type: 'ADD_TO_READING_LIST',
+        payload
+    }
+}
+
+export function removeFromReadingList(payload){
+    return {
+        type: 'REMOVE_FROM_READING_LIST',
+        payload
+    }
+}
+
